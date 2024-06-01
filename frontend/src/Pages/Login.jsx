8ed1af9c7932537366d9b47f85be3bc3a87ec846
Dowman137 <dominith456@gmail.com>
2024-06-01 17:21:59 -0500

    se ha creado un componente PopUp y se ha utilizado en Login para mostrar un mensaje de error

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -3,10 +3,12 @@ import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth, db } from '../Firebase/config';
 import { Link, useNavigate } from 'react-router-dom';
 import { getDoc, doc } from 'firebase/firestore';
+import PopUp from '../components/PopUp';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
@@ -25,18 +27,20 @@ const Login = () => {
           // Redirigir al usuario al chat con el nombre como parámetro
           navigate(`/inicio?name=${encodeURIComponent(userName)}`);
         } else {
-          console.error("Error logging in: User document does not exist");
+          console.error("Error logging in: User document does not exist");       
         }
       } else {
         console.error("Error logging in: User is not authenticated");
       }
     } catch (error) {
       console.error("Error logging in:", error);
+      setShowPopup(true);
     }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      {showPopup && <PopUp onClose={() => setShowPopup(false)}/>}
       <form className="bg-white p-6 rounded shadow-md w-full max-w-sm text-center" onSubmit={handleLogin}>
         <h1 className="text-2xl font-bold mb-4">Iniciar sesión</h1>
         <input
